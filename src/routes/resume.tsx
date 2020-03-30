32f1d3913ec91a5610b603ac4c0a1eaec4cdf27c
Mark Lewis <22421135+marklewis01@users.noreply.github.com>
2020-03-30 21:53:19 +1100

    moved Project above Projects - trying to troubleshoot deployment issues.

--- src/routes/resume.tsx
+++ src/routes/resume.tsx
@@ -20,6 +20,90 @@ import {
 
 // data
 import { experience, projects, skills, IProject } from "../data";
+
+const Project = ({ project }: { project: IProject }) => {
+  const [lightBox, setLightBox] = useState(false);
+  const [galleryIndex, setGalleryIndex] = useState(0);
+
+  const toggleLightbox = (selectedIndex: number) => {
+    setLightBox(!lightBox);
+    setGalleryIndex(selectedIndex);
+  };
+  return (
+    <Grid container className="project">
+      <Grid item xs={12} lg="auto" className="lhs-col">
+        <Grid container className="details">
+          <Grid item xs>
+            <h3>{project.label}</h3>
+          </Grid>
+          <Grid item xs container className="links">
+            {project.web ? (
+              <a href={project.web} target="_blank">
+                <Grid container alignItems="center">
+                  <WebsiteIcon className="icon" />
+                  <Typography>Live Site</Typography>
+                </Grid>
+              </a>
+            ) : null}
+            {project.codebase ? (
+              <a href={project.codebase} target="_blank">
+                <Grid container alignItems="center">
+                  <CodeIcon className="icon" />
+                  <Typography>Source</Typography>
+                </Grid>
+              </a>
+            ) : null}
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} lg container>
+        <Typography variant="body1">
+          {project.description}
+          {project.keywords.length ? (
+            <span className="keywords">
+              {" "}
+              ({project["keywords"].join(", ")})
+            </span>
+          ) : null}
+        </Typography>
+      </Grid>
+
+      {project.images ? (
+        <Grid item xs={12} lg>
+          <Grid container spacing={2}>
+            {project.images.map(({ caption, source }, j) => (
+              <Grid
+                item
+                xs={4}
+                sm={3}
+                onClick={() => toggleLightbox(j)}
+                key={source.thumbnail}
+              >
+                <img
+                  alt={caption}
+                  src={source.thumbnail}
+                  className="image-thumb"
+                />
+              </Grid>
+            ))}
+          </Grid>
+          <ModalGateway>
+            {lightBox ? (
+              <Modal allowFullscreen={false} onClose={() => setLightBox(false)}>
+                <Carousel
+                  currentIndex={galleryIndex}
+                  frameProps={{ autoSize: "height" }}
+                  views={project.images}
+                />
+              </Modal>
+            ) : null}
+          </ModalGateway>
+        </Grid>
+      ) : null}
+    </Grid>
+  );
+};
+
 export const Resume = ({ path }: { path: string }) => {
   return (
     <Grid className="resume">
@@ -184,89 +268,6 @@ export const Resume = ({ path }: { path: string }) => {
           ))}
         </Grid>
       </Box>
-    </Grid>
-  );
-};
-
-const Project = ({ project }: { project: IProject }) => {
-  const [lightBox, setLightBox] = useState(false);
-  const [galleryIndex, setGalleryIndex] = useState(0);
-
-  const toggleLightbox = (selectedIndex: number) => {
-    setLightBox(!lightBox);
-    setGalleryIndex(selectedIndex);
-  };
-  return (
-    <Grid container className="project">
-      <Grid item xs={12} lg="auto" className="lhs-col">
-        <Grid container className="details">
-          <Grid item xs>
-            <h3>{project.label}</h3>
-          </Grid>
-          <Grid item xs container className="links">
-            {project.web ? (
-              <a href={project.web} target="_blank">
-                <Grid container alignItems="center">
-                  <WebsiteIcon className="icon" />
-                  <Typography>Live Site</Typography>
-                </Grid>
-              </a>
-            ) : null}
-            {project.codebase ? (
-              <a href={project.codebase} target="_blank">
-                <Grid container alignItems="center">
-                  <CodeIcon className="icon" />
-                  <Typography>Source</Typography>
-                </Grid>
-              </a>
-            ) : null}
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} lg container>
-        <Typography variant="body1">
-          {project.description}
-          {project.keywords.length ? (
-            <span className="keywords">
-              {" "}
-              ({project["keywords"].join(", ")})
-            </span>
-          ) : null}
-        </Typography>
-      </Grid>
-
-      {project.images ? (
-        <Grid item xs={12} lg>
-          <Grid container spacing={2}>
-            {project.images.map(({ caption, source }, j) => (
-              <Grid
-                item
-                xs={4}
-                sm={3}
-                onClick={() => toggleLightbox(j)}
-                key={source.thumbnail}
-              >
-                <img
-                  alt={caption}
-                  src={source.thumbnail}
-                  className="image-thumb"
-                />
-              </Grid>
-            ))}
-          </Grid>
-          <ModalGateway>
-            {lightBox ? (
-              <Modal allowFullscreen={false} onClose={() => setLightBox(false)}>
-                <Carousel
-                  currentIndex={galleryIndex}
-                  frameProps={{ autoSize: "height" }}
-                  views={project.images}
-                />
-              </Modal>
-            ) : null}
-          </ModalGateway>
-        </Grid>
-      ) : null}
     </Grid>
   );
 };
